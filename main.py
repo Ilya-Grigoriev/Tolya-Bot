6bf9322e9c69120162f73bb5367c234433b0c535
Ilya-Grigoriev <iliagrigoryev@yandex.ru>
2022-03-29 21:26:41 +0300

    Добавлен конвертер валют

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
 import requests
 from datetime import datetime
+import traceback
+
+
+def clear_data(context):
+    context.user_data['amount'] = None
+    context.user_data['from_cur'] = None
+    context.user_data['to_cur'] = None
+
+
+def start_keyboard():
+    reply_keyboard = [['/forecast', '/converter_currency']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    return markup
 
 
 def first_response(update, context):
-    update.message.reply_text('Введите название города:')
+    if update.message['text'] == '/converter_currency':
+        update.message.reply_text('Введите количество денег:')
+    else:
+        update.message.reply_text('Введите название города:')
     return 1
 
 
 def start(update, context):
-    reply_keyboard = [['/forecast']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    with open('currencies.txt', encoding='utf8') as file_1:
+        context.user_data['currencies'] = [i.strip('\n') for i in file_1.readlines()]
     update.message.reply_text(
         f'Привет, {update.message["chat"]["first_name"]}. Меня зовут Толя! У меня следующий набор функций:',
-        reply_markup=markup)
+        reply_markup=start_keyboard())
 
 
+# Переводчик
 def translate(text, from_lang, to_lang):
     url = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get"
     direct = f'{from_lang}|{to_lang}'
@@ -29,46 +46,109 @@ def translate(text, from_lang, to_lang):
     return response['responseData']['translatedText']
 
 
+# Конвертер валют
+def set_amount(update, context):
+    try:
+        context.user_data['amount'] = float(update.message['text'])
+        update.message.reply_text('\n'.join(context.user_data['currencies']))
+        reply_keyboard = [[i.split()[0]] for i in context.user_data['currencies']]
+        markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
+        update.message.reply_text('Выберите валюту, из который переводите деньги:', reply_markup=markup)
+        return 2
+    except Exception:
+        update.message.reply_text('Не удалось обработать запрос', reply_markup=start_keyboard())
+        print(traceback.format_exc())
+        return ConversationHandler.END
+
+
+def set_from_cur(update, context):
+    try:
+        cur = update.message['text']
+        if cur in ' '.join(context.user_data['currencies']):
+            context.user_data['from_cur'] = cur
+            update.message.reply_text('Выберите валюты, в которую переводите деньги:')
+            return 3
+        else:
+            raise Exception
+    except Exception:
+        update.message.reply_text('Не удалось обработать запрос', reply_markup=start_keyboard())
+        print(traceback.format_exc())
+    clear_data(context)
+    return ConversationHandler.END
+
+
+def set_to_cur(update, context):
+    try:
+        cur = update.message['text']
+        if cur in ' '.join([i.split()[0] for i in context.user_data['currencies']]):
+            context.user_data['to_cur'] = cur
+            amount = context.user_data['amount']
+            from_cur = context.user_data['from_cur']
+            url = "https://currency-exchange.p.rapidapi.com/exchange"
+            params = {"to": cur, "from": from_cur}
+            headers = {
+                "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+                "X-RapidAPI-Key": "5ba360e216mshff693a6a557ef28p19c45bjsne7791971c241"
+            }
+            response = requests.request("GET", url, headers=headers, params=params)
+            update.message.reply_text(f'{float(response.text) * amount} {cur}', reply_markup=start_keyboard())
+        else:
+            raise Exception
+    except Exception:
+        update.message.reply_text('Не удалось обработать запрос', reply_markup=start_keyboard())
+        print(traceback.format_exc())
+    clear_data(context)
+    return ConversationHandler.END
+
+
+# Прогноз погоды
 def forecast(update, context):
-    dict_wind = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
-                 'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'c': 'штиль'}
-    place = update.message['text']
-    apikey = '40d1649f-0493-4b70-98ba-98533de7710b'
-    geocoder_request = f'http://geocode-maps.yandex.ru/1.x/?apikey={apikey}&geocode="{place}"&format=json'
-    response = requests.get(geocoder_request)
-    if response:
-        json_response = response.json()
-        pos = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]['Point']['pos']
-        lon, lat = pos.split()
-    else:
-        print("Ошибка выполнения запроса:")
-        print(geocoder_request)
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-    headers = {'lat': lat, 'lon': lon, 'lang': 'ru_RU',
-               'X-Yandex-API-Key': '9009f2a9-7220-4bb5-be28-0fad1d330b93'}
-    response = requests.get('https://api.weather.yandex.ru/v2/forecast?', headers=headers).json()
-    ts = int(response['now'])
-    date = datetime.utcfromtimestamp(ts).strftime('%d.%m.%Y')
-    temp = response['fact']['temp']
-    feels_temp = response['fact']['feels_like']
-    condition = translate(response['fact']['condition'], 'en', 'ru')
-    pressure = response['fact']['pressure_mm']
-    humidity = response['fact']['humidity']
-    wind_speed = response['fact']['wind_speed']
-    wind_dir = dict_wind[response['fact']['wind_dir']]
-    update.message.reply_text(f'Прогноз погоды сегодня в городе {place.capitalize()} на {date}:')
-    update.message.reply_text(f'Температура: {temp}℃')
-    update.message.reply_text(f'Ощущаемая температура: {feels_temp}℃')
-    update.message.reply_text(f'Погодное описание: {condition.lower()}')
-    update.message.reply_text(f'Давление: {pressure} мм')
-    update.message.reply_text(f'Влажность: {humidity}%')
-    update.message.reply_text(f'Скорость ветра: {wind_speed} м/с')
-    update.message.reply_text(f'Направление ветра: {wind_dir}')
+    reply_keyboard = [['/forecast', '/converter_currency']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    try:
+        dict_wind = {'nw': 'северо-западное', 'n': 'северное', 'ne': 'северо-восточное', 'e': 'восточное',
+                     'se': 'юго-восточное', 's': 'южное', 'sw': 'юго-западное', 'w': 'западное', 'c': 'штиль'}
+        place = update.message['text']
+        apikey = '40d1649f-0493-4b70-98ba-98533de7710b'
+        geocoder_request = f'http://geocode-maps.yandex.ru/1.x/?apikey={apikey}&geocode="{place}"&format=json'
+        response = requests.get(geocoder_request)
+        if response:
+            json_response = response.json()
+            pos = json_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]['Point']['pos']
+            lon, lat = pos.split()
+        else:
+            print("Ошибка выполнения запроса:")
+            print(geocoder_request)
+            print("Http статус:", response.status_code, "(", response.reason, ")")
+        headers = {'lat': lat, 'lon': lon, 'lang': 'ru_RU',
+                   'X-Yandex-API-Key': '9009f2a9-7220-4bb5-be28-0fad1d330b93'}
+        response = requests.get('https://api.weather.yandex.ru/v2/forecast?', headers=headers).json()
+        ts = int(response['now'])
+        date = datetime.utcfromtimestamp(ts).strftime('%d.%m.%Y')
+        temp = response['fact']['temp']
+        feels_temp = response['fact']['feels_like']
+        condition = translate(response['fact']['condition'], 'en', 'ru')
+        pressure = response['fact']['pressure_mm']
+        humidity = response['fact']['humidity']
+        wind_speed = response['fact']['wind_speed']
+        wind_dir = dict_wind[response['fact']['wind_dir']]
+        update.message.reply_text(f'Прогноз погоды сегодня в городе {place.capitalize()} на {date}:')
+        update.message.reply_text(f'Температура: {temp}℃')
+        update.message.reply_text(f'Ощущаемая температура: {feels_temp}℃')
+        update.message.reply_text(f'Погодное описание: {condition.lower()}')
+        update.message.reply_text(f'Давление: {pressure} мм')
+        update.message.reply_text(f'Влажность: {humidity}%')
+        update.message.reply_text(f'Скорость ветра: {wind_speed} м/с')
+        update.message.reply_text(f'Направление ветра: {wind_dir}', reply_markup=markup)
+    except Exception:
+        update.message.reply_text('Не удалось обработать ваш запрос', reply_markup=start_keyboard())
     return ConversationHandler.END
 
 
 def stop(update, context):
-    update.message.reply_text('Программа завершена', reply_markup=ReplyKeyboardRemove())
+    reply_keyboard = [['/forecast', '/converter_currency']]
+    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+    update.message.reply_text('Программа завершена', reply_markup=markup)
     return ConversationHandler.END
 
 
@@ -76,15 +156,25 @@ def main():
     token = '5147228144:AAG-lIcg7-YZJqpJ5gfHZrR_J6hBtAZomO0'
     updater = Updater(token)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', start))
-    conv_handler = ConversationHandler(
+    dp.add_handler(CommandHandler('start', start, pass_user_data=True))
+    forecast_handler = ConversationHandler(
         entry_points=[CommandHandler('forecast', first_response)],
         states={
             1: [MessageHandler(Filters.text & (~ Filters.command), forecast)]
         },
         fallbacks=[CommandHandler('stop', stop)]
     )
-    dp.add_handler(conv_handler)
+    dp.add_handler(forecast_handler)
+    converter_handler = ConversationHandler(
+        entry_points=[CommandHandler('converter_currency', first_response, pass_user_data=True)],
+        states={
+            1: [MessageHandler(Filters.text & (~ Filters.command), set_amount, pass_user_data=True)],
+            2: [MessageHandler(Filters.text & (~ Filters.command), set_from_cur, pass_user_data=True)],
+            3: [MessageHandler(Filters.text & (~ Filters.command), set_to_cur, pass_user_data=True)]
+        },
+        fallbacks=[CommandHandler('stop', stop)]
+    )
+    dp.add_handler(converter_handler)
     updater.start_polling()
     updater.idle()
 
