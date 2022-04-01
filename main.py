941dbf5517853a66f2ddc02e4b21e62cfb31ba8e
Ilya-Grigoriev <iliagrigoryev@yandex.ru>
2022-04-01 16:46:25 +0300

    Project

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler, ConversationHandler
 from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove, Bot
 import requests
 from datetime import datetime
